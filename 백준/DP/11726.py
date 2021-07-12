9ff9a72c22f60863671c008683b9615fe4e7edda
jinho <gogod23@ajou.ac.kr>
2021-07-12 19:48:19 +0900

    dp

--- 백준/DP/11726.py
+++ 백준/DP/11726.py
@@ -3,12 +3,14 @@
 
 
 n = int(input())
-dp = [0]*(n+1)
-dp[0] = 0
-dp[1] = 1
-dp[2] = 2
+if n == 1 or n == 2:
+    print(n)
+else:
+    dp = [0] * (n + 1)
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n+1):
+        dp[i] = dp[i-1]+dp[i-2]
 
-for i in range(3, n+1):
-    dp[i] = dp[i-1]+dp[i-2]
-
-print(dp[-1] % 10007)
+    print(dp[-1] % 10007)
