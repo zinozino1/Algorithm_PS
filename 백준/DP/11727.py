87829fd4dab98ec410e035856a5071cef91ac809
jinho <gogod23@ajou.ac.kr>
2021-07-12 20:08:59 +0900

    dp

---
+++ 백준/DP/11727.py
@@ -0,0 +1,16 @@
+# 실버3 - 2*n 타일링 2
+
+n = int(input())
+if n == 1:
+    print(1)
+elif n == 2:
+    print(3)
+else:
+    dp = [0] * (n + 1)
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 3
+    for i in range(3, n+1):
+        dp[i] = dp[i-1]+2*dp[i-2]
+
+    print(dp[-1] % 10007)
