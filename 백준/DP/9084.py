f4c0d96089700e93fda39c881a4532c796a432e8
jinho <gogod23@ajou.ac.kr>
2021-08-05 11:33:26 +0900

    dp

--- 백준/DP/9084.py
+++ 백준/DP/9084.py
@@ -22,3 +22,19 @@ for _ in range(T):
             dp[i][j] = tmp
 
     print(dp[n][target])
+
+# 개선된 풀이 -> 누적합이라는 것이 중요
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    coins = list(map(int, input().split()))
+    m = int(input())
+    dp = [0]*(m+1)
+    dp[0] = 1
+
+    for i in range(1, n+1):
+        for j in range(coins[i-1], m+1):
+            dp[j] += dp[j-coins[i-1]]
+
+    print(dp[m])
