c5a60df277f2677c1a5380aecbd931ae8f837260
jinho <gogod23@ajou.ac.kr>
2021-09-09 22:54:04 +0900

    binary search

--- 백준/이분탐색/2512.py
+++ 백준/이분탐색/2512.py
@@ -1,41 +1,43 @@
-# 2512-실버3-이분탐색
-# n = 10만이므로 완전탐색 불가능
-# 전형적인 이분탐색 유형(갯수, 합)중에서 합 문제 -> 갯수문제는 capa 갯수 판단해야 하기 때문에 좀 더 어려움
+# 실버3 - 예산
+# 이분탐색
+
+# 접근 :
+# 1. tmp를 1부터 total 까지 설정한 다음에 tmp에 대해 budget 루프롤 돌린다
+# -> 10만 * 1만이므로 TLE
+# 2. 이분탐색 -> 10만짜리 탐색을 이분탐색으로 log10만 으로 만든다.
 
 
-import sys
-INF = sys.maxsize
-input = sys.stdin.readline
+# 전형적 이분탐색 문제.
+# rt를 잡을 때 10만이나, total로 잡으면 안된다. 모든 지방의 예산 금액 합이 M을 초과하지 않는다면 답은 지방의 요구 금액 최댓값이기 때문
 
 
-def sol():
 
-    n = int(input())
-    target = list(map(int, input().split()))
-    total = int(input())
+n = int(input())
+budget = list(map(int, input().split()))
+total = int(input())
 
-    lt = 1
-    rt = max(target)
+lt = 1
+rt = max(budget)
 
-    def count(capa):
-        tmp = []
-        for x in target:
-            if x >= capa:
-                tmp.append(capa)
-            else:
-                tmp.append(x)
-        return sum(tmp)
 
-    res = 0
-    while lt <= rt:
-        mid = (lt + rt) // 2
-        if count(mid) <= total:
-            lt = mid + 1
-            res = mid
+def judge(capa):
+    tmp = 0
+    for b in budget:
+        if b > capa:
+            tmp += capa
         else:
-            rt = mid - 1
+            tmp += b
+    return True if tmp <= total else False
 
-    print(res)
 
+res = 0
 
-sol()
+while lt <= rt:
+    mid = (lt + rt) // 2
+    if judge(mid):
+        lt = mid + 1
+        res = mid
+    else:
+        rt = mid - 1
+
+print(res)
