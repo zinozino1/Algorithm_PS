219a6d3a2598f015ad975e3335a7bab79f04e840
jinho <gogod23@ajou.ac.kr>
2021-09-23 21:11:28 +0900

    stack

---
+++ 백준/자료구조 활용/2493.py
@@ -0,0 +1,26 @@
+# 골드5-탑
+# 스택 활용
+
+# 스택에 내림차순으로 쌓으면 O(N)이다
+
+n = int(input())
+towers = list(map(int, input().split()))
+stack = [(towers[0], 0)]
+
+max_idx = 0
+max_val = towers[0]
+res = [0]*n
+
+for i in range(1, n):
+
+    while stack and stack[-1][0] < towers[i]:
+        stack.pop()
+    if stack and stack[-1][0] > towers[i]:
+        res[i] = stack[-1][1]+1
+
+    stack.append((towers[i], i))
+    if max_val > towers[i]:
+        max_val = towers[i]
+        max_idx = i
+
+print(*res)
